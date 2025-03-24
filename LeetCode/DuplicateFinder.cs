eff7994816e542efce32eb2a71d81c59cef00d32
Filip <filip.benishek@gmail.com>
2025-03-24 19:24:55 +0100

    Add project files

---
+++ LeetCode/DuplicateFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+public class DuplicateFinder
+{
+    public static int[] FindDuplicates(int[] nums)
+    {
+        Dictionary<int, int> countMap = new Dictionary<int, int>();
+        List<int> duplicates = new List<int>();
+        foreach (int num in nums)
+        {
+            if (countMap.ContainsKey(num))
+            {
+                countMap[num]++;
+            }
+            else
+            {
+                countMap[num] = 1;
+            }
+        }
+        foreach (var kvp in countMap)
+        {
+            if (kvp.Value == 2)
+            {
+                duplicates.Add(kvp.Key);
+            }
+        }
+        return duplicates.ToArray();
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] nums1 = { 0, 1, 1, 0 };
+        int[] nums2 = { 0, 3, 2, 1, 3, 2 };
+        int[] nums3 = { 7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2 };
+
+        Console.WriteLine($"Output: [{string.Join(", ", DuplicateFinder.FindDuplicates(nums1))}]");
+        Console.WriteLine($"Output: [{string.Join(", ", DuplicateFinder.FindDuplicates(nums2))}]");
+        Console.WriteLine($"Output: [{string.Join(", ", DuplicateFinder.FindDuplicates(nums3))}]");
+    }
+}
